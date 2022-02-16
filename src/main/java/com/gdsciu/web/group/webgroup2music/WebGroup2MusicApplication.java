7375745ed649c95abb56385eaa3073596a1b9a3a
nlthanh-gdsciu <nlthanh.gdsciu.gmail.com>
2022-02-17 01:55:27 +0700

    initialize project

---
+++ src/main/java/com/gdsciu/web/group/webgroup2music/WebGroup2MusicApplication.java
@@ -0,0 +1,13 @@
+package com.gdsciu.web.group.webgroup2music;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebGroup2MusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebGroup2MusicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gdsciu/web/group/webgroup2music/WebGroup2MusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdsciu.web.group.webgroup2music;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebGroup2MusicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
