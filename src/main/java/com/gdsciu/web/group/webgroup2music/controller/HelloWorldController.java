088088280242dd5e6f175333526560056a449e2f
nlthanh-gdsciu <nlthanh.gdsciu.gmail.com>
2022-02-17 23:43:45 +0700

    Add HelloWorldController

---
+++ src/main/java/com/gdsciu/web/group/webgroup2music/controller/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.gdsciu.web.group.webgroup2music.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloWorldController {
+    @GetMapping(value = {"/", "/helloWorld"})
+    public String displayHelloWorld() {
+        return "hello-word";
+    }
+}
